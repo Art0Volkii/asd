be80d0201efa5385f8618b321e58fb7f7bf99d1a
Art0Volkii <100345483+Art0Volkii@users.noreply.github.com>
2025-03-11 21:28:09 +0200

    Update t04_05.py

--- 3-5/t04_05.py
+++ 3-5/t04_05.py
@@ -1,22 +1,22 @@
-import math
-
 def f(x):
     '''
-    Функція f(x) на відрізку [1.6 , 3] незростаюча. 
+    Функція f(x) на відрізку [0, 2] дуже швидко зростаюча.
     '''
-    return math.sin(x) - (x / 3)
+    return x**3 + 4*x**2 + x - 6
 
 def binary_search(a,b):
     l = a
     r = b
-    eps = 1e-10
-    while r - l > eps:
+    eps = 1e-9
+    while abs(r - l) > eps:
         m = (l + r) / 2.0
-        if f(m) > 0:
+        if f(m) < 0:
             l = m
         else:
             r = m
     return (l + r) / 2
 
-x = binary_search(1.6, 3)
+x = binary_search(0, 2)
 print(f"{x:.9f}")
+
+#RES:  1.000000000
