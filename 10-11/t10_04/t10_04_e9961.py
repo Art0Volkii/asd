3c35fc56dc30e8613db8240ef417b9858e72e594
Art0Volkii <100345483+Art0Volkii@users.noreply.github.com>
2025-04-04 21:07:32 +0300

    Create t10_04_e9961.py

---
+++ 10-11/t10_04/t10_04_e9961.py
@@ -0,0 +1,17 @@
+def get_permutations(n, k, curr_perm, used):
+    if len(curr_perm) == k:
+        print(" ".join(map(str, curr_perm)))
+        return
+
+    for i in range(1, n + 1):
+        if not used[i]:
+
+            used[i] = True
+            curr_perm.append(i)
+            get_permutations(n, k, curr_perm, used)
+            curr_perm.pop()
+            used[i] = False
+
+n, k = map(int, input().split())
+
+get_permutations(n, k, [], [False] * (n + 1))
