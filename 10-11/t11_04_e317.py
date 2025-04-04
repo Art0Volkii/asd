577e4b1168ccfc3d378212d2b7608de2c9f42a74
Art0Volkii <100345483+Art0Volkii@users.noreply.github.com>
2025-04-04 21:14:19 +0300

    Create t11_04_e317.py

---
+++ 10-11/t11_04_e317.py
@@ -0,0 +1,22 @@
+def karacuba_mul(A, B):
+
+    n, m = len(A), len(B)
+    res = [0] * (n + m) 
+
+    A = [int(d) for d in A][::-1]
+    B = [int(d) for d in B][::-1]
+
+    for i in range(n):
+        for j in range(m):
+            res[i + j] += A[i] * B[j]
+            res[i + j + 1] += res[i + j] // 10
+            res[i + j] %= 10
+
+    while len(res) > 1 and res[-1] == 0:
+        res.pop()
+
+    return ''.join(map(str, res[::-1]))  
+
+A, B = input().split()
+
+print(karacuba_mul(A, B))
